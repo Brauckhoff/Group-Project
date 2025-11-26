3a422079fca20812eb9c069369d07646c3aa9588
Brauckhoff <Tim.Brauckhoff@b-tu.de>
2025-11-26 19:42:56 +0100

    wrote script to download data

---
+++ test_scripts/fetch_sra.py
@@ -0,0 +1,29 @@
+import subprocess
+import sys
+
+
+# based on https://erilu.github.io/python-fastq-downloader/#downloading-fastq-files-using-the-sra-toolkit
+
+def get_sra(*args):
+    """
+    Function to download SRA-files and extract fastq from NCBI
+    :param args: SRR-ID(s) (space separated)
+    :return: .sra and fastq.gz files <- that's what we want
+    """
+    for arg in args:
+
+        # this will download the .sra files to ~/ncbi/public/sra/ (will create directory if not present)
+        print("Currently downloading: " + arg)
+        prefetch = "prefetch " + arg
+        print("The command used was: " + prefetch)
+        subprocess.call(prefetch, shell=True)
+
+        # this will extract the .sra files from above into a folder named 'fastq'
+        print("Generating fastq for: " + arg)
+        fastq_dump = "fastq-dump --outdir fastq --gzip --skip-technical  --readids --read-filter pass --dumpbase --split-3 --clip ~/ncbi/public/sra/" + arg + ".sra"
+        print("The command used was: " + fastq_dump)
+        subprocess.call(fastq_dump, shell=True)
+
+if __name__ == "__main__":
+    # get all arguments after script
+    get_sra(*sys.argv[1:])
