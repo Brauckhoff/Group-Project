4fd4f5fad830b6e96cf1392f857090dace714724
Brauckhoff <Tim.Brauckhoff@b-tu.de>
2025-12-31 09:07:37 +0100

    small script correction

--- test_scripts/circularity.sh
+++ test_scripts/circularity.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-for d in assembled/ERR* assembled/SRR* assembled/co*; do
+for d in assembled_flye/ERR* assembled_flye/SRR* assembled_flye/hum*; do
    
-   contig="$d/contigs.fasta.gz"
+   # MDBG: for d in assembled/ERR* assembled/SRR* assembled/co*; do
+   
+   # MDBG: contig="$d/contigs.fasta.gz"
+   contig="$d/assembly.fasta"
    ID=$(basename -- "$d")
    
-   echo	"Starting with $ID"
+   echo "Starting with $ID"
    
-   python3 MetaMDBG_Manuscript-main/run_singleContigs.py /teachstor/share/groupprojectWS25/groupC/assembled/circular_contigs/$ID $contig $contig mdbg 16
+   # MDBG
+   #python3 MetaMDBG_Manuscript-main/run_singleContigs.py /teachstor/share/groupprojectWS25/groupC/assembled/circular_contigs/$ID /teachstor/share/groupprojectWS25/groupC/$contig /teachstor/share/groupprojectWS25/groupC/$contig mdbg 16
+   
+   # METAFLYE
+   python3 MetaMDBG_Manuscript-main/run_singleContigs.py /teachstor/share/groupprojectWS25/groupC/assembled_flye/circular_contigs/$ID /teachstor/share/groupprojectWS25/groupC/$contig /teachstor/share/groupprojectWS25/groupC/$d/assembly_info.txt metaflye 16
+   
    echo "Done with $ID"
 done
+
